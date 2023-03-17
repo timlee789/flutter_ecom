a41a708a76600b677227951f57707168cd04976e
timlee789 <84083360+timlee789@users.noreply.github.com>
2023-03-17 17:43:30 -0400

    0317-2

---
+++ lib/models/product_model.dart
@@ -0,0 +1,72 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+
+  const Product({
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.price,
+    required this.isPopular,
+    required this.isRecommended,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props =>
+      [name, category, imageUrl, price, isPopular, isRecommended];
+
+  static List<Product> product = [
+    const Product(
+      name: 'Soft Drink #1',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #2',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #3',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #4',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #1',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+  ];
+}
